fe44e7e33209e3bf158be49b77071841d1baa403
Marcin Lempart <marcin.lempart@gmail.com>
2021-10-04 23:38:01 +0200

    4 commit

--- app/src/main/java/com/example/kafeteriacoffeina/MainActivity.java
+++ app/src/main/java/com/example/kafeteriacoffeina/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kafeteriacoffeina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-            }
-        });
+    }
+
+    public void onShowDetails(View view){
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kafeteriacoffeina/Workout.java
+++ app/src/main/java/com/example/kafeteriacoffeina/Workout.java
@@ -1,2 +1,32 @@
-package com.example.kafeteriacoffeina;public class Workout {
+package com.example.kafeteriacoffeina;
+
+public class Workout {
+    private String name;
+    private String descritpion;
+
+    public static final Workout[] workouts = {
+            new Workout("Rozciąganie kończyn", "5 pompek w staniu na rękack"),
+            new Workout("Tylko dla mięczaków", "100 pompek")
+    };
+
+    public Workout(String name, String descritpion) {
+        this.name = name;
+        this.descritpion = descritpion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescritpion() {
+        return descritpion;
+    }
+
+    public void setDescritpion(String descritpion) {
+        this.descritpion = descritpion;
+    }
 }
